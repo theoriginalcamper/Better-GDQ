5886b432db177fd4bc750a9b34a8e843509d23c2
TheOriginalCamper <theoriginalcamper@users.noreply.github.com>
2016-07-04 16:41:42 -0400

    Version 1.4.0 - Add Youtube links for VODs from the GDQ Youtube account

--- app/scripts/vod-contentscript.js
+++ app/scripts/vod-contentscript.js
@@ -13,11 +13,21 @@ function addVodLinks() {
 
 		$.each(titles, function (index, title) {
 			console.log(title);
-			var hrefString = "https://www.twitch.tv/gamesdonequick/v/" + data[title]["video-link"] + "?t=" + data[title]["time"];
-			var templateString = "<a href=\"" + hrefString + "\" class=\"vod-link\">" + title + " - <i class=\"fa fa-check\"></i> COMPLETED</a>";
+			var templateString = generateVodString(title, data);
 			$("td:contains(" + title + "):first").html(templateString);
 		});
 
 		console.log("Done");
 	});
+}
+
+function generateVodString(title, data) {
+	var twitchString = "https://www.twitch.tv/gamesdonequick/v/" + data[title]["video-link"] + "?t=" + data[title]["time"];
+
+	if (typeof data[title]["youtube"] != 'undefined') {
+		var youtubeString = data[title]["youtube"];
+		return title + " - <i class=\"fa fa-check\"></i> COMPLETED <a href=\"" + twitchString + "\" class=\"vod-link\"><i class=\"fa fa-twitch\"></i></a> <a href=\"" + youtubeString + "\" class=\"vod-link\"><i class=\"fa fa-youtube\"></i></a>";
+	} else {
+		return "<a href=\"" + twitchString + "\" class=\"vod-link\">" + title + " - <i class=\"fa fa-check\"></i> COMPLETED <i class=\"fa fa-twitch\"></i> Twitch Only</a>";
+	}
 }
