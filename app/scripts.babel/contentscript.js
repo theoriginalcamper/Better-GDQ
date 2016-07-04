8daa750bb6f96deeefd91d9ef5100fbcc1ac9f37
TheOriginalCamper <theoriginalcamper@users.noreply.github.com>
2016-07-03 20:52:59 -0400

    Version 1.0.0

---
+++ app/scripts.babel/contentscript.js
@@ -0,0 +1,330 @@
+'use strict';
+
+// Timer Variables
+
+var refreshTimer = null;
+var refreshRate = 300000;
+
+var port = chrome.runtime.connect({name: "gdq"});
+
+var page_elem = document.querySelector('body');
+console.log(page_elem);
+
+var runners_paragraph = document.createElement('p');
+var game_link_a = document.createElement('a'); 
+
+runners_paragraph.id = "gdq-runners-information";
+game_link_a.id = "gdq-speedrun-link";
+game_link_a.className = "speedrun-link";
+
+$.get(chrome.extension.getURL('/html/gdq-footer.html'), function(data) {
+    $($.parseHTML(data)).appendTo(page_elem);
+});
+
+$.get(chrome.extension.getURL('/html/settings-menu.html'), function(data) {
+    var menu_ul = $(".settings-menu");
+    $($.parseHTML(data)).appendTo(menu_ul);
+});
+
+$(page_elem).on('click', 'ul.dropdown-menu', function (e) {
+  e.stopPropagation();
+});
+
+
+
+$(document).ready(function() {
+    $(page_elem).css('margin-bottom', '70px');
+    $('.game-information').append(game_link_a);
+    $('.game-information').append(runners_paragraph);
+
+    $("#refresh-timer-update").on('submit', function(e) {
+        e.preventDefault();
+
+        var updateRefreshTimerValue = $('#refresh-timer-update').serializeArray()[0]["value"];
+
+        console.log(updateRefreshTimerValue);
+        updateRefreshRate(updateRefreshTimerValue);
+
+        return false;
+    });
+
+    $("#schedule-items-update").on('submit', function(e) {
+        e.preventDefault();
+
+        var updateScheduleItemsValue = $('#schedule-items-update').serializeArray()[0]["value"];
+
+        console.log(updateScheduleItemsValue);
+        sendUpdateCalendarItemsNumber(updateScheduleItemsValue);
+
+        return false;
+    });
+
+    $(".fa.fa-refresh").click(function() {
+        this.className = 'fa fa-refresh fa-spin';
+        console.log("Refreshing...");
+        requestDataFromBackground();
+        var that = this;
+        setTimeout(function() {
+            that.className = 'fa fa-refresh';
+            console.log("Refresh complete.")
+        }, 2000);
+    });
+
+    $(".fa.fa-cog").click(function() {
+        this.className = 'fa fa-cog fa-spin';
+        var that = this;
+        setTimeout(function() {
+            that.className = 'fa fa-cog';
+        }, 2000);
+    });
+
+    $("[name='quakenet-chat-switch']").bootstrapSwitch();
+
+    $('input[name="quakenet-chat-switch"]').on('switchChange.bootstrapSwitch', function(event, state) {
+        console.log('Clicked QUAKENET Switch.');
+
+        if ($(".bootstrap-switch").hasClass('bootstrap-switch-on')) {
+            console.log('Switch is on. Adding Chat iframe and modifying UI.');
+            $('#stream').removeClass('center-block').addClass('pull-left');
+            $.get(chrome.extension.getURL('/html/quakenet-chat.html'), function(data) {
+                var twitchStream = $("#stream");
+                $(twitchStream).after($.parseHTML(data));
+            });
+        } else {
+            console.log('Switch is off. Removing UI.');
+            $('#stream').addClass('center-block').removeClass('pull-left');
+            $('#quakenet-chat').remove();
+            $('#quakenet-clear').remove();
+        }
+    });
+});
+
+//Port Listeners
+
+var port = chrome.runtime.connect({name: "gdq"});
+port.postMessage({message: "request"});
+port.onMessage.addListener(function(msg) {
+    if (msg.status == "changed") {
+        console.log(msg);
+        updateUI(msg);
+        updateCalendarUI(msg.calendar);
+    } else if (msg.status == "unchanged") {
+        console.log("Current game has not changed since last request");
+    } else if (msg.status == "reload") {
+        console.log("Reload has occurred");
+        console.log(msg);
+        updateUI(msg);
+        updateCalendarUI(msg.calendar);
+    }
+});
+
+/*
+  Object
+    Game
+    Runner
+    Estimate
+    Category
+    Link
+*/
+
+function updateUI(msg) {
+    runners_paragraph.innerHTML = generateFormattedRunnerString(msg.runner);
+    game_link_a.href = msg.link;
+    game_link_a.onclick = function() {
+        window.open(this.href); 
+        return false;
+    }
+
+    if (msg.category != null) {
+        game_link_a.innerHTML = msg.game + ' (' + msg.category +')';
+    } else {
+        game_link_a.innerHTML = msg.game;
+    }
+}
+
+function updateCalendarUI(msg) {
+    console.log("Calendar updating...");
+    console.log(msg);
+    if (msg != null) {
+        $('#schedule-table tbody').empty();
+        
+        var scheduleString = "";
+        _.each(msg.order, function(gameTitle, index) {
+            scheduleString += generateScheduleItemString(msg.schedule[gameTitle], index + 1);
+        });
+
+        $('#schedule-table tbody').html(scheduleString);
+        console.log("Calendar updated.")
+    } else {
+        return;
+    }
+}
+
+function generateFormattedRunnerString(runners) {
+    var runners_keys = _.keys(runners);
+    var runner_string = "by ";
+    if (runners_keys.length > 2) {
+        var last_runner = runners_keys.pop()
+        var  second_runner = runners_keys.pop();
+        $.each(runners_keys, function(index, runner_key) {
+            runner_string += generateRunnerElement(runners, runner_key) + ', ';
+        });
+
+        runner_string += generateRunnerElement(runners, second_runner) + ' ';
+        runner_string += 'and '
+        runner_string += generateRunnerElement(runners, last_runner);
+    } else if (runners_keys.length == 2) {
+        var last_runner = runner_keys.pop()
+        var  second_runner = runner_keys.pop();
+        
+        runner_string += generateRunnerElement(runners, second_runner);
+        runner_string += ' and ';
+        runner_string += generateRunnerElement(runners, last_runner);
+    } else if (runners_keys.length == 1) {
+        var runner_key = runners_keys[0];
+        runner_string += generateRunnerElement(runners, runners_keys[0]);
+    } else {
+        console.log("Error no runners.")
+        runner_string = "";
+    }
+
+    return runner_string;
+}
+
+function generateRunnerElement(runnerObject, runner_key) {
+    if (runnerObject[runner_key]["logo"] == null) {
+        return `<a href="${runnerObject[runner_key]["link"]}" onclick="window.open(this.href); return false;">${runner_key}</a>`;
+    } else {
+        return `<a href="${runnerObject[runner_key]["link"]}" onclick="window.open(this.href); return false;"><img class="runner-logo" src="${runnerObject[runner_key]["logo"]}" />${runner_key}</a>`;
+    }
+}
+
+function generateScheduleItemString(scheduleItemObject, index) {
+    var runnerString = generateFormattedRunnerString(scheduleItemObject.runner);
+    if (scheduleItemObject.category != null) {
+        var titleString = scheduleItemObject.title + ' (' + scheduleItemObject.category +')';
+    } else {
+        var titleString = scheduleItemObject.title;
+    }
+    
+    var scheduleItemString = `<tr>
+                                <th scope="row">${index}</th>
+                                <td>
+                                    <a class="speedrun-link" id="next-game-title" href="${scheduleItemObject.link}"> ${titleString}</a>
+                                    <p class="runners-links" id="next-runners-information">${runnerString}</p>
+                                </td>
+                                <td style="width: 114px;">
+                                    <p class="text-right"><i class="fa fa-clock-o" aria-hidden="true"></i> ${scheduleItemObject.estimate}</p>
+                                </td>
+                              </tr>`;
+
+
+    return scheduleItemString;
+}
+
+function calculateRefreshRate(minutes) {
+    // body...
+    var milliseconds = minutes * 60000;
+
+    return milliseconds;
+}
+
+function updateRefreshRate(newRate) {
+    if (newRate == "Set Refresh Timer:") {
+        console.log("No change in refresh timer value.");
+
+        $.notify({
+            icon: 'glyphicon glyphicon-warning-sign',
+            title: 'Refresh Timer Update:',
+            message: 'Please choose an option from the menu!',
+        },{
+        // settings
+            element: 'body',
+            position: null,
+            type: "danger",
+            allow_dismiss: true,
+            newest_on_top: true,
+            showProgressbar: false,
+            placement: {
+                from: "top",
+                align: "right"
+            }
+        });
+
+        return;
+    } else if (typeof parseInt(newRate) == "number" && parseInt(newRate) > 0) {
+        clearInterval(refreshTimer);
+        var milliseconds = calculateRefreshRate(parseInt(newRate));
+        setInterval(requestDataFromBackground, milliseconds);
+        console.log("Refresh Timer has been updated to: " + newRate + " minutes or " + milliseconds + " milliseconds");
+
+        $.notify({
+            icon: 'glyphicon glyphicon-time',
+            title: 'Refresh Timer Update:',
+            message: 'Updated to refresh every ' + newRate + ' minutes',
+        },{
+        // settings
+            element: 'body',
+            position: null,
+            type: "success",
+            allow_dismiss: true,
+            newest_on_top: true,
+            showProgressbar: false,
+            placement: {
+                from: "top",
+                align: "right"
+            }
+        });
+    }
+}
+
+function sendUpdateCalendarItemsNumber(newCalendarItemsNumber) {
+    if (newCalendarItemsNumber == "Set # of Runs to Display:") {
+        console.log("No change in numbers of runs to display in schedule.");
+
+        $.notify({
+            icon: 'glyphicon glyphicon-warning-sign',
+            title: 'Schedule Items Update',
+            message: 'Please choose an option from the menu!',
+        },{
+        // settings
+            element: 'body',
+            position: null,
+            type: "danger",
+            allow_dismiss: true,
+            newest_on_top: true,
+            showProgressbar: false,
+            placement: {
+                from: "top",
+                align: "right"
+            }
+        });
+
+        return;
+    } else if (typeof parseInt(newCalendarItemsNumber) == "number" && parseInt(newCalendarItemsNumber) > 0) {
+        port.postMessage({message: "schedule", calendarItemsNumber: parseInt(newCalendarItemsNumber)});
+
+        $.notify({
+            icon: 'glyphicon glyphicon-time',
+            title: 'Schedule Items Update:',
+            message: 'Updated to display ' + newCalendarItemsNumber + ' runs.',
+        },{
+        // settings
+            element: 'body',
+            position: null,
+            type: "success",
+            allow_dismiss: true,
+            newest_on_top: true,
+            showProgressbar: false,
+            placement: {
+                from: "top",
+                align: "right"
+            }
+        });
+    }
+}
+
+function requestDataFromBackground() {
+    port.postMessage({message: "request"});
+}
+refreshTimer = setInterval(requestDataFromBackground, refreshRate);
---
+++ app/scripts/background.js
@@ -0,0 +1,154 @@
+'use strict';
+
+// chrome.runtime.onInstalled.addListener(details => {
+//   console.log('previousVersion', details.previousVersion);
+// });
+
+var current_game = null;
+var current_game_title = null;
+var current_link = null;
+var current_runners = null;
+var current_estimate = null;
+var current_category = null;
+var current_calendar = null;
+var current_number_calendar_items = 4;
+
+var runnerJSON = null;
+var scheduleJSON = null;
+
+var portForMessage = null;
+
+$.getJSON('/json/sgdq_runners.json').done(function (resp) {
+    runnerJSON = resp;
+});
+$.getJSON('/json/sgdq_schedule.json').done(function (resp) {
+    scheduleJSON = resp;
+});
+
+chrome.runtime.onConnect.addListener(function (port) {
+    portForMessage = port;
+    console.assert(port.name == "gdq");
+    if (port.name == "gdq") {
+        port.onMessage.addListener(function (msg) {
+            if (msg.message == "request") {
+                $.getJSON("https://api.twitch.tv/channels/gamesdonequick").done(function (resp) {
+                    console.log("Completed request to Twitch");
+
+                    if (current_game != resp.game) {
+                        console.log("The Current Game being run is: " + resp.game);
+
+                        current_game = resp.game;
+                        getSpeedrunData(current_game, port);
+                        console.log(current_link);
+                    } else {
+                        console.log("Still the same");
+                    }
+                });
+            } else if (msg.message == "schedule") {
+                console.log(msg.calendarItemsNumber);
+                current_number_calendar_items = msg.calendarItemsNumber;
+                getSpeedrunData(current_game, port);
+            };
+        });
+    }
+});
+
+function getSpeedrunData(game, port) {
+    var gameData = scheduleJSON[game];
+
+    if (typeof gameData != 'undefined') {
+        current_game = game;
+        current_game_title = gameData.title;
+        current_runners = getRunnerData(gameData.runner);
+        current_estimate = gameData.estimate;
+        current_category = gameData.category;
+        current_link = gameData.link;
+
+        var next_games = [];
+        var game_index = _.keys(scheduleJSON).indexOf(current_game);
+
+        console.log(game_index);
+
+        var next_game = null;
+        current_calendar = {};
+        var schedule_object = {};
+
+        if (171 - game_index >= current_number_calendar_items) {
+            _(current_number_calendar_items).times(function (index) {
+                next_game = _.keys(scheduleJSON)[game_index + index + 1];
+
+                next_games.push(next_game);
+            });
+        } else {
+            _(171 - game_index).times(function (index) {
+                next_game = _.keys(scheduleJSON)[game_index + index + 1];
+
+                next_games.push(next_game);
+            });
+        }
+
+        console.log(next_games);
+        var schedule_object = _.reduce(next_games, function (object, gameTitle) {
+            object[gameTitle] = $.extend({}, scheduleJSON[gameTitle]);
+            object[gameTitle]["runner"] = getRunnerData($.extend({}, scheduleJSON[gameTitle])["runner"]);
+            return object;
+        }, {});
+
+        current_calendar["order"] = next_games;
+        current_calendar["schedule"] = schedule_object;
+
+        console.log(current_calendar);
+
+        console.log("Game Data for " + current_game_title + " has been retrieved.");
+
+        port.postMessage({ status: "changed",
+            game: current_game_title,
+            runner: current_runners,
+            estimate: current_estimate,
+            category: current_category,
+            link: current_link,
+            calendar: current_calendar
+        });
+
+        console.log("Game Data sent!");
+    } else {
+        console.log("Current game cannot be found in the parsed schedule.");
+        console.log("Returned a value of undefined.");
+    }
+}
+
+function getRunnerData(runners) {
+    var runnersArray = runners.split(", ");
+
+    var runnersObject = _.reduce(runnersArray, function (object, runner) {
+        var runnerData = runnerJSON[runner];
+        object[runner] = { logo: runnerData.logo, link: runnerData.link };
+        return object;
+    }, {});
+
+    return runnersObject;
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.url.indexOf("https://gamesdonequick.com") > -1 && changeInfo.url === undefined) {
+        portForMessage.postMessage({ status: "reload",
+            game: current_game_title,
+            runner: current_runners,
+            estimate: current_estimate,
+            category: current_category,
+            link: current_link,
+            calendar: current_calendar });
+    }
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.url.indexOf("http://gamesdonequick.com") > -1 && changeInfo.url === undefined) {
+        portForMessage.postMessage({ status: "reload",
+            game: current_game_title,
+            runner: current_runners,
+            estimate: current_estimate,
+            category: current_category,
+            link: current_link,
+            calendar: current_calendar });
+    }
+});
