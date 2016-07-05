ab42ea29d16671fc216d27d44ba8b299442115c7
TheOriginalCamper <theoriginalcamper@users.noreply.github.com>
2016-07-04 20:47:13 -0400

    Version 1.5.0 - Add ability to embed Twitch player above message pane on the GDQ Discord web interface

---
+++ app/scripts.babel/discord-contentscript.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	console.log("Hello Discord!")
+
+	console.log($('.messages-wrapper'));
+
+	var checkMessagesContainer = setInterval(function(){
+    	if ($(".messages-wrapper").length > 0){ // Check if element has been found
+	      	console.log('Add CSS to Messages Panel');
+	      	$('.messages-wrapper').css('height', '400px');
+	      	clearInterval(checkMessagesContainer);
+	    }
+    },1000);
+
+    var checkLinks = setInterval(function(){
+    	if ($(".links").length > 0){ // Check if element has been found
+	      	console.log('Add Switch to Links Panel');
+	      	$('.links').html('<label for="twitch-player-display" id="twitch-player-display-label">Twitch Player Embed</label>');
+	      	$('.links').append(`<input type="checkbox" data-size="mini" name="twitch-player-display">`);
+	      	clearInterval(checkLinks);
+
+	      	$("[name='twitch-player-display']").bootstrapSwitch();
+	      	console.log($("[name='twitch-player-display']").bootstrapSwitch('state'));
+
+	      	$('input[name="twitch-player-display"]').on('switchChange.bootstrapSwitch', function(event, state) {
+		        console.log('Clicked Twitch Switch.');
+		        console.log($("[name='twitch-player-display']").bootstrapSwitch('state'));
+		        if ($('input[name="twitch-player-display"]').bootstrapSwitch('state')) {
+		            updateTwitchPlayer('add');
+		        } else {
+		            updateTwitchPlayer('remove');
+		        }
+		    });
+		}
+    },1000);
+
+    function updateTwitchPlayer(msg) {
+        if(msg == 'add') {
+            console.log('Switch is on. Adding Twitch iframe and modifying UI.');
+            $('.messages-wrapper').before('<iframe id="twitch-embed" src="https://player.twitch.tv/?channel=gamesdonequick" style="margin: auto;" width="560" height="315" frameborder="0" scrolling="no" allowFullscreen="true" class="center-block"></iframe>');
+        } else if (msg == 'remove') {
+            console.log('Switch is off. Removing Twitch iframe and UI changes.');
+            $('#twitch-embed').remove();
+        }
+    }
+});
