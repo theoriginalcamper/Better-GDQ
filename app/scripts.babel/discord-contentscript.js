360458b900a2ec9de7fd042b5044096272e172ce
TheOriginalCamper <theoriginalcamper@users.noreply.github.com>
2016-07-06 08:55:51 -0400

    Version 1.8.0 - Added Theater mode for Discord with accurate functionality switching between channels

--- app/scripts.babel/discord-contentscript.js
+++ app/scripts.babel/discord-contentscript.js
@@ -1,27 +1,17 @@
 $(document).ready(function() {
-	console.log("Hello Discord!")
+	console.log("Hello Discord!");
 
 	console.log($('.messages-wrapper'));
 
 	var userListStatus = null;
-
-	var checkMessagesContainer = setInterval(function(){
-    	if ($(".messages-wrapper").length > 0){ // Check if element has been found
-	      	console.log('Add CSS to Messages Panel');
-	      	$('.messages-wrapper').css('height', '400px');
-	      	clearInterval(checkMessagesContainer);
-	    }
-    },1000);
+	var twitchActive = false;
+	
 
     var checkLinks = setInterval(function(){
     	if ($(".topic").length > 0){ // Check if element has been found
 	      	console.log('Add Switch to Links Panel');
-	      	$('.topic').after('<div id="twitch-switch"><label for="twitch-player-display" id="twitch-player-display-label">Twitch Player Embed</label></div>');
-	      	$('#twitch-switch').append(`<input type="checkbox" data-size="mini" name="twitch-player-display">`);
+	      	addTwitchSwitch();
 	      	clearInterval(checkLinks);
-
-	      	$("[name='twitch-player-display']").bootstrapSwitch();
-	      	console.log($("[name='twitch-player-display']").bootstrapSwitch('state'));
 
 	      	$('input[name="twitch-player-display"]').on('switchChange.bootstrapSwitch', function(event, state) {
 		        console.log('Clicked Twitch Switch.');
@@ -35,6 +25,13 @@ $(document).ready(function() {
 		}
     },1000);
 
+    function addTwitchSwitch() {
+    	$('.links').html('<div id="twitch-switch"><label for="twitch-player-display" id="twitch-player-display-label">Twitch Player Embed</label></div>');
+	    $('#twitch-switch').append(`<input type="checkbox" data-size="mini" name="twitch-player-display">`);
+	      	
+	    $("[name='twitch-player-display']").bootstrapSwitch();
+    }
+
     function updateTwitchPlayer(msg) {
         if(msg == 'add') {
             console.log('Switch is on. Adding Twitch iframe and modifying UI.');	
@@ -47,22 +44,64 @@ $(document).ready(function() {
             }
 
             $('button.active').click();
-            $('.messages-wrapper').parent().css('align-items', 'flex-end');
-            $('.messages-wrapper').css('width', '48%');
-            $('.messages-wrapper').next('form').css({'width': '46%', 'margin-right': '2%', 'margin-left': '0px'});
 
-            $('.app').before(`<div id="twitch-container" style="width: ${$(document).width() - $('.guilds-wrapper').width() - $('.messages-wrapper').width()}px; height: ${$(document).height() - $('.title-wrap').outerHeight()}px; position: fixed; z-index:100; top: ${$('.title-wrap').outerHeight()}px; left: ${$('.guilds-wrapper').width()}px;"><iframe id="twitch-embed" src="https://player.twitch.tv/?channel=gamesdonequick" width="100%" height="100%" frameborder="0" scrolling="no" allowFullscreen="true" class="center-block"></iframe></div>`);
+            twitchActive = true;
+
+            updateDiscordUI('add');
+
+            $('.app').before(`<div id="twitch-container" style="width: ${$(document).width() - $('.guilds-wrapper').width() - $('.messages-wrapper').width()}px; height: ${$(document).height() - $('.title-wrap').outerHeight() - $('.links').outerHeight()}px; position: fixed; z-index:100; top: ${$('.title-wrap').outerHeight()}px; left: ${$('.guilds-wrapper').width()}px;"><iframe id="twitch-embed" src="https://player.twitch.tv/?channel=gamesdonequick" width="100%" height="100%" frameborder="0" scrolling="no" allowFullscreen="true" class="center-block"></iframe></div>`);
         	
         } else if (msg == 'remove') {
             console.log('Switch is off. Removing Twitch iframe and UI changes.');
             $('#twitch-container').remove();
-            $('.messages-wrapper').parent().removeAttr('style');
-            $('.messages-wrapper').removeAttr('style');
-            $('.messages-wrapper').next('form').removeAttr('style');
+            twitchActive = false;
+            updateDiscordUI('remove');
 
             if(userListStatus == true) {
             	$('.header-toolbar button:nth-child(2)').click();
             }
+            
         }
     }
+
+    function updateDiscordUI(msg) {
+    	console.log("TwitchActive: " + twitchActive);
+    	if (msg == 'add') {
+    		console.log("Rearranging Discord UI");
+			$('.messages-wrapper').parent().css('align-items', 'flex-end');
+            $('.messages-wrapper').css('width', '48%');
+            $('.messages-wrapper').next('form').css({'width': '46%', 'margin-right': '2%', 'margin-left': '0px'});
+    	} else if (msg == 'remove') {
+    		console.log("Discord UI returned to normal");
+    		$('.messages-wrapper').parent().removeAttr('style');
+            $('.messages-wrapper').removeAttr('style');
+            $('.messages-wrapper').next('form').removeAttr('style');
+    	}
+    }
+
+    var checkForActiveGuild = setInterval(function() {
+    	if ($(".selected").length > 0) { // Check if element has been found
+    		$('.guild').on('click', function() {
+    			if (twitchActive) {
+    				if ($(this).has('a[href="/channels/140605087511740416/140605087511740416"]').length > 0) {
+    					$('#twitch-container').css('display', '');
+            			var uiUpdate = setInterval(function() {
+            				if($('.guild-header').length > 0 && $('.guild-header header span').text() == 'GamesDoneQuick') {
+
+            					updateDiscordUI('add');
+            					clearInterval(uiUpdate);
+            				}
+            			});
+    				} else {
+    					$('#twitch-container').css('display', 'none');
+            			updateDiscordUI('remove');
+    				}
+    			}
+
+    		});
+
+    		clearInterval(checkForActiveGuild);
+		}
+    },1000);
+
 });
--- app/scripts/discord-contentscript.js
+++ app/scripts/discord-contentscript.js
@@ -1,71 +1,106 @@
 "use strict";
 
 $(document).ready(function () {
-  console.log("Hello Discord!");
+    console.log("Hello Discord!");
 
-  console.log($('.messages-wrapper'));
+    console.log($('.messages-wrapper'));
 
-  var userListStatus = null;
+    var userListStatus = null;
+    var twitchActive = false;
 
-  var checkMessagesContainer = setInterval(function () {
-    if ($(".messages-wrapper").length > 0) {
-      // Check if element has been found
-      console.log('Add CSS to Messages Panel');
-      $('.messages-wrapper').css('height', '400px');
-      clearInterval(checkMessagesContainer);
-    }
-  }, 1000);
+    var checkLinks = setInterval(function () {
+        if ($(".topic").length > 0) {
+            // Check if element has been found
+            console.log('Add Switch to Links Panel');
+            addTwitchSwitch();
+            clearInterval(checkLinks);
 
-  var checkLinks = setInterval(function () {
-    if ($(".topic").length > 0) {
-      // Check if element has been found
-      console.log('Add Switch to Links Panel');
-      $('.topic').after('<div id="twitch-switch"><label for="twitch-player-display" id="twitch-player-display-label">Twitch Player Embed</label></div>');
-      $('#twitch-switch').append("<input type=\"checkbox\" data-size=\"mini\" name=\"twitch-player-display\">");
-      clearInterval(checkLinks);
-
-      $("[name='twitch-player-display']").bootstrapSwitch();
-      console.log($("[name='twitch-player-display']").bootstrapSwitch('state'));
-
-      $('input[name="twitch-player-display"]').on('switchChange.bootstrapSwitch', function (event, state) {
-        console.log('Clicked Twitch Switch.');
-        console.log($("[name='twitch-player-display']").bootstrapSwitch('state'));
-        if ($('input[name="twitch-player-display"]').bootstrapSwitch('state')) {
-          updateTwitchPlayer('add');
-        } else {
-          updateTwitchPlayer('remove');
+            $('input[name="twitch-player-display"]').on('switchChange.bootstrapSwitch', function (event, state) {
+                console.log('Clicked Twitch Switch.');
+                console.log($("[name='twitch-player-display']").bootstrapSwitch('state'));
+                if ($('input[name="twitch-player-display"]').bootstrapSwitch('state')) {
+                    updateTwitchPlayer('add');
+                } else {
+                    updateTwitchPlayer('remove');
+                }
+            });
         }
-      });
+    }, 1000);
+
+    function addTwitchSwitch() {
+        $('.links').html('<div id="twitch-switch"><label for="twitch-player-display" id="twitch-player-display-label">Twitch Player Embed</label></div>');
+        $('#twitch-switch').append("<input type=\"checkbox\" data-size=\"mini\" name=\"twitch-player-display\">");
+
+        $("[name='twitch-player-display']").bootstrapSwitch();
     }
-  }, 1000);
 
-  function updateTwitchPlayer(msg) {
-    if (msg == 'add') {
-      console.log('Switch is on. Adding Twitch iframe and modifying UI.');
+    function updateTwitchPlayer(msg) {
+        if (msg == 'add') {
+            console.log('Switch is on. Adding Twitch iframe and modifying UI.');
 
-      if ($('.header-toolbar button:nth-child(2)').hasClass('active')) {
-        userListStatus = true;
-        $('.header-toolbar button:nth-child(2)').click();
-      } else {
-        userListStatus = false;
-      }
+            if ($('.header-toolbar button:nth-child(2)').hasClass('active')) {
+                userListStatus = true;
+                $('.header-toolbar button:nth-child(2)').click();
+            } else {
+                userListStatus = false;
+            }
 
-      $('button.active').click();
-      $('.messages-wrapper').parent().css('align-items', 'flex-end');
-      $('.messages-wrapper').css('width', '48%');
-      $('.messages-wrapper').next('form').css({ 'width': '46%', 'margin-right': '2%', 'margin-left': '0px' });
+            $('button.active').click();
 
-      $('.app').before("<div id=\"twitch-container\" style=\"width: " + ($(document).width() - $('.guilds-wrapper').width() - $('.messages-wrapper').width()) + "px; height: " + ($(document).height() - $('.title-wrap').outerHeight()) + "px; position: fixed; z-index:100; top: " + $('.title-wrap').outerHeight() + "px; left: " + $('.guilds-wrapper').width() + "px;\"><iframe id=\"twitch-embed\" src=\"https://player.twitch.tv/?channel=gamesdonequick\" width=\"100%\" height=\"100%\" frameborder=\"0\" scrolling=\"no\" allowFullscreen=\"true\" class=\"center-block\"></iframe></div>");
-    } else if (msg == 'remove') {
-      console.log('Switch is off. Removing Twitch iframe and UI changes.');
-      $('#twitch-container').remove();
-      $('.messages-wrapper').parent().removeAttr('style');
-      $('.messages-wrapper').removeAttr('style');
-      $('.messages-wrapper').next('form').removeAttr('style');
+            twitchActive = true;
 
-      if (userListStatus == true) {
-        $('.header-toolbar button:nth-child(2)').click();
-      }
+            updateDiscordUI('add');
+
+            $('.app').before("<div id=\"twitch-container\" style=\"width: " + ($(document).width() - $('.guilds-wrapper').width() - $('.messages-wrapper').width()) + "px; height: " + ($(document).height() - $('.title-wrap').outerHeight() - $('.links').outerHeight()) + "px; position: fixed; z-index:100; top: " + $('.title-wrap').outerHeight() + "px; left: " + $('.guilds-wrapper').width() + "px;\"><iframe id=\"twitch-embed\" src=\"https://player.twitch.tv/?channel=gamesdonequick\" width=\"100%\" height=\"100%\" frameborder=\"0\" scrolling=\"no\" allowFullscreen=\"true\" class=\"center-block\"></iframe></div>");
+        } else if (msg == 'remove') {
+            console.log('Switch is off. Removing Twitch iframe and UI changes.');
+            $('#twitch-container').remove();
+            twitchActive = false;
+            updateDiscordUI('remove');
+
+            if (userListStatus == true) {
+                $('.header-toolbar button:nth-child(2)').click();
+            }
+        }
     }
-  }
+
+    function updateDiscordUI(msg) {
+        console.log("TwitchActive: " + twitchActive);
+        if (msg == 'add') {
+            console.log("Rearranging Discord UI");
+            $('.messages-wrapper').parent().css('align-items', 'flex-end');
+            $('.messages-wrapper').css('width', '48%');
+            $('.messages-wrapper').next('form').css({ 'width': '46%', 'margin-right': '2%', 'margin-left': '0px' });
+        } else if (msg == 'remove') {
+            console.log("Discord UI returned to normal");
+            $('.messages-wrapper').parent().removeAttr('style');
+            $('.messages-wrapper').removeAttr('style');
+            $('.messages-wrapper').next('form').removeAttr('style');
+        }
+    }
+
+    var checkForActiveGuild = setInterval(function () {
+        if ($(".selected").length > 0) {
+            // Check if element has been found
+            $('.guild').on('click', function () {
+                if (twitchActive) {
+                    if ($(this).has('a[href="/channels/140605087511740416/140605087511740416"]').length > 0) {
+                        $('#twitch-container').css('display', '');
+                        var uiUpdate = setInterval(function () {
+                            if ($('.guild-header').length > 0 && $('.guild-header header span').text() == 'GamesDoneQuick') {
+
+                                updateDiscordUI('add');
+                                clearInterval(uiUpdate);
+                            }
+                        });
+                    } else {
+                        $('#twitch-container').css('display', 'none');
+                        updateDiscordUI('remove');
+                    }
+                }
+            });
+
+            clearInterval(checkForActiveGuild);
+        }
+    }, 1000);
 });
